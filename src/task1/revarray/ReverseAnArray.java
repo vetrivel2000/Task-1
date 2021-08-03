09bdc64483e2a0dfdef29ee31348fe54cfbe606b
vetrivel2000 <vetrivel1924@gmail.com>
2021-08-03 13:57:24 +0530

    Initial commit

---
+++ src/task1/revarray/ReverseAnArray.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vetri
+ */
+package task1.revarray;
+public class ReverseAnArray {
+    public void reverse(int array[],int size)
+    {
+        int array2[]=new int[size];
+        int index=size-1;
+        for(int i=0;i<size;i++)
+        {
+            array2[index]=array[i];
+            index--;
+        }
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(array2[i]+" ");
+        }
+    }
+}
---
+++ src/task1/sum/SumOfValues.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vetri
+ */
+package task1.sum;
+public class SumOfValues {
+    public int findSum(int array[],int size)
+    {
+        int sum=0;
+        for(int i=0;i<size;i++)
+        {
+            sum = sum+array[i];
+        }
+        return sum;
+    }
+}
